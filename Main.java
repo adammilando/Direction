8950596c935bf20c1a835e6292ff64b061926108
Adam <adam.milando71@gmail.com>
2023-02-15 13:24:23 +0700

    [improve]

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Robot robot = new Robot(new Position(2,3),Direction.EAST);
 
-        String commands = "AARRALA";
+        String commands = "AARAAAA";
         for (int i = 0; i < commands.length();i++){
             robot.move(commands.charAt(i));
             robot.tampil();
